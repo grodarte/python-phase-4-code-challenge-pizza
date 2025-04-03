58ca1be40e8e10d1c815e26ad5de7064b04272b8
Gabrielle <ms.grodarte@gmail.com>
2025-04-03 13:38:44 -0700

    will add validations

--- server/models.py
+++ server/models.py
@@ -70,6 +70,11 @@ class RestaurantPizza(db.Model, SerializerMixin):
     serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if price not in range(1,31):
+            raise ValueError("Price must be between 1-30.")
+        return price
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
